7947a1fdac2954e1b180ed62b05285cb94df2b8e
JediL1C1OUS <ben.fobar@gmail.com>
2015-05-06 14:49:13 -0400

    Committed by Udacity

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,83 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Benjamin Fobar on 5/4/15.
+//  Copyright (c) 2015 Ben Fobar. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+
+    var audioPlayer:AVAudioPlayer!
+    var receivedAudio:RecordedAudio!
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
+        audioPlayer.enableRate = true
+        
+        audioEngine = AVAudioEngine()
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func playAudio(rate: Float) {
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        audioPlayer.rate = rate
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
+    }
+    
+    @IBAction func playAudioSlow(sender: UIButton) {
+        playAudio(0.5)
+    }
+
+    @IBAction func playAudioFast(sender: UIButton) {
+        playAudio(1.5)
+    }
+    
+    @IBAction func playChipmunkAudio(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
+    }
+    
+    @IBAction func playVaderAudio(sender: UIButton) {
+        playAudioWithVariablePitch(-1000)
+    }
+    
+    func playAudioWithVariablePitch(pitch: Float){
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        
+        audioPlayerNode.play()
+    }
+    
+    @IBAction func stopPlaying(sender: UIButton) {
+        audioPlayer.stop()
+    }
+}
---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,16 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Benjamin Fobar on 5/4/15.
+//  Copyright (c) 2015 Ben Fobar. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+    
+    override init() {}
+}
